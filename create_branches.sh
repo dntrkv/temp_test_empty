2b971479f275f41a8dce95a79d4aeaccba3ad36d
Dan Turkov <danturkov@gmail.com>
2021-06-09 19:47:15 -0700

    change

--- create_branches.sh
+++ create_branches.sh
@@ -3,13 +3,9 @@
 GIT=`which git`
 TEMP_DIR="./temp"
 
-counter=10
+counter=3271
 
-if [ -d "$TEMP_DIR" ]; then rm -Rf $TEMP_DIR; fi
-
-mkdir $TEMP_DIR
-
-while [ $counter -lt 1000 ]
+while [ $counter -lt 10000 ]
 do
   branch_name="branch-$counter"
 
@@ -18,6 +14,8 @@ do
   ${GIT} checkout -b $branch_name
 
   touch "$TEMP_DIR/$counter"
+
+  echo "$counter - this is some data" > $TEMP_DIR/$counter
 
   ${GIT} add --all .
   ${GIT} commit -m "Test commit $counter"
