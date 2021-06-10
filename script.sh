78024651cef2f01720f1fdea8fb5e05a75af5f4c
Dan Turkov <danturkov@gmail.com>
2021-06-09 17:13:44 -0700

    first script

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+GIT=`which git`
+TEMP_DIR="./temp"
+
+counter=0
+
+if [ -d "$TEMP_DIR" ]; then rm -Rf $TEMP_DIR; fi
+
+mkdir $TEMP_DIR
+
+while [ $counter -lt 3 ]
+do
+  branch_name="branch-$counter"
+
+  echo "Creating branch: $branch_name"
+
+  ${GIT} checkout -b $branch_name
+
+  touch "$TEMP_DIR/$counter"
+
+  ${GIT} add --all .
+  ${GIT} commit -m "Test commit $counter"
+  ${GIT} push origin $branch_name
+
+  counter=`expr $counter + 1`
+done
+
+
